172fe0ef404cb1292d4fe13088c4be0805a8e607
cristianocsl <cslcristiano@gmail.com>
2021-08-16 19:22:30 -0300

    exercicio 1

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,59 +1,56 @@
 // src/Cars.jsx
 
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import CarsContext from './context/CarsContext';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
 
-class Cars extends Component {
-  render() {
-    const { redCar, blueCar, yellowCar } = this.context.cars;
-    const { moveCar } = this.context;
-    return (
+function Cars () {
+  const { carColor, carColor: {redCar, blueCar, yellowCar} , setCarColor } = useContext(CarsContext);
+  return (
+    <div>
       <div>
-        <div>
-          <img
-            className={redCar ? 'car-right' : 'car-left'}
-            src={carRed}
-            alt="red car"
-          />
-          <button
-            onClick={() => moveCar('redCar', !redCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
-        <div>
-          <img
-            className={blueCar ? 'car-right' : 'car-left'}
-            src={carBlue}
-            alt="blue car"
-          />
-          <button
-            onClick={() => moveCar('blueCar', !blueCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
-        <div>
-          <img
-            className={yellowCar ? 'car-right' : 'car-left'}
-            src={carYellow}
-            alt="yellow car"
-          />
-          <button
-            onClick={() => moveCar('yellowCar', !yellowCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
+        <img
+          className={redCar ? 'car-right' : 'car-left'}
+          src={carRed}
+          alt="red car"
+        />
+        <button
+          onClick={() => setCarColor({...carColor, redCar: !redCar})}
+          type="button"
+        >
+          Move
+      </button>
       </div>
-    )
-  }
+      <div>
+        <img
+          className={blueCar ? 'car-right' : 'car-left'}
+          src={carBlue}
+          alt="blue car"
+        />
+        <button
+          onClick={() => setCarColor({...carColor, blueCar: !blueCar})}
+          type="button"
+        >
+          Move
+      </button>
+      </div>
+      <div>
+        <img
+          className={yellowCar ? 'car-right' : 'car-left'}
+          src={carYellow}
+          alt="yellow car"
+        />
+        <button
+          onClick={() => setCarColor({...carColor, yelllowCar: !yellowCar})}
+          type="button"
+        >
+          Move
+      </button>
+      </div>
+    </div>
+  )
 };
 
 Cars.contextType = CarsContext;
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,45 +1,23 @@
 // src/context/Provider.js
 
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CarsContext from './CarsContext';
 
-class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: {
-        red: false,
-        blue: false,
-        yellow: false,
-      },
-    }
-    this.moveCar = this.moveCar.bind(this);
-  }
+function Provider ({ children }) {
+  
+  const [carColor, setCarColor] = useState({
+    redCar: false,
+    blueCar: false,
+    yellowCar: false,
+  });
 
-  moveCar(car, side) {
-    this.setState({
-      cars: {
-        ...this.state.cars,
-        [car]: side,
-      },
-    });
-  };
-
-  render() {
-    const context = {
-      ...this.state,
-      moveCar: this.moveCar,
-    };
-
-    const { children } = this.props;
-
-    return (
-      <CarsContext.Provider value={context}>
-        {children}
-      </CarsContext.Provider>
-    );
-  }
+  const data = { carColor, setCarColor };
+  return (
+    <CarsContext.Provider value={data}>
+      {children}
+    </CarsContext.Provider>
+  );
 };
 
 Provider.propTypes = {
